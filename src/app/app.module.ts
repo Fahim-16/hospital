7aee3180fdf3eb2782d3eb83ec6e2541eb7e2ee1
Fahim-16 <fahim.sakeer@gmail.com>
2022-07-08 01:53:09 +0530

    Doctor view created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ViewdocComponent } from './viewdoc/viewdoc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    ViewdocComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewdoc/viewdoc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewdoc',
+  templateUrl: './viewdoc.component.html',
+  styleUrls: ['./viewdoc.component.css']
+})
+export class ViewdocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
